059c03f785b55c14155aaf9731a277e0a081e9ef
Tatiana <temees@bk.ru>
2021-06-11 16:23:42 +0300

    3les

--- calc.go
+++ calc.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var a float64
+	var a, b, r float64
+	var op string
+
+	fmt.Print("Input a: ")
 	if _, err := fmt.Scanln(&a); err != nil {
+		fmt.Println("a must be numeric")
 		return
 	}
 
+	fmt.Print("Input b: ")
+	if _, err := fmt.Scanln(&b); err != nil {
+		fmt.Println("b must be numeric")
+		return
+	}
+
+	fmt.Print("Input op: ")
+	if _, err := fmt.Scanln(&op); err != nil {
+
+		return
+	}
+
+	switch op {
+	case "+":
+		r = a + b
+	case "-":
+		r = a - b
+	case "*":
+		r = a * b
+	case "/":
+		r = a / b
+
+	case "^":
+		r = math.Pow(a, b)
+	default:
+		fmt.Println("Operation must be +, -, /, *, ^")
+		return
+	}
+	fmt.Println(a, op, b, "=", r)
 }
