b33846d72b9ec608cc7dd1bbc420d1ffc40f3685
Tatiana <temees@bk.ru>
2021-07-10 17:28:07 +0300

    10les

---
+++ mas/mas.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var num string
+	fmt.Println("Input numerics with comma: ")
+	fmt.Scanln(&num)
+	mas := strings.Split(num, ",")
+	var sliceNum []int
+	for _, s := range mas {
+		n, err := strconv.Atoi(s)
+		if err == nil {
+			sliceNum = append(sliceNum, n)
+		}
+	}
+
+	InsertionSort(sliceNum)
+	fmt.Println(sliceNum)
+}
+func InsertionSort(ar []int) []int {
+	for i := 1; i < len(ar); i++ {
+		x := ar[i]
+		j := i
+		for ; j >= 1 && ar[j-1] > x; j-- {
+			ar[j] = ar[j-1]
+		}
+		ar[j] = x
+	}
+	return ar
+}
